a69858e0ae221dd61b67b7e5abdd9f36d0ea2526
swethaannam05 <annamswetharani@gmail.com>
2023-07-02 16:45:47 -0400

    Average of 3 students

--- beeCrowd/Program.cs
+++ beeCrowd/Program.cs
@@ -11,24 +11,40 @@ namespace beeCrowd
     {
         static void Main(string[] args)
         {
-            
+            #region "student Grade Average of 3 "
+            StudentGradeAverage2 avg1 = new StudentGradeAverage2();
+            avg1.Average1();
+            #endregion
+
+            #region "student Grade Average of 2 "
             StudentGradeAverage avg = new StudentGradeAverage();
             avg.Average();
+            #endregion
 
+            #region "Sum problem with output prod"
             Prod prod = new Prod();
             prod.Prod1();
+            #endregion
 
+            #region "Addtion with output soma"
             SomaAdd somaAdd = new SomaAdd();
             somaAdd.SomaAddition();
+            #endregion
 
+            #region "Area of circle"
             Circle circle = new Circle();
             circle.Area();
+            #endregion
 
+            #region "Addition problem"
             ExtreamlyBasic extreamlyBasic = new ExtreamlyBasic();
             extreamlyBasic.A();
+            #endregion
 
+            #region "HEllo world program"
             Console.WriteLine("Hello World");
             Console.ReadLine();
+            #endregion
         }
     }
 }
---
+++ beeCrowd/StudentGradeAverage2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beeCrowd
+{
+    public class StudentGradeAverage2
+    {
+        public double Average1()
+        {
+            Console.WriteLine("Enter grade of student 1");
+            double studs1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter grade of student 2");
+            double studs2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter grade of student 3");
+            double studs3=Convert.ToDouble(Console.ReadLine());
+            double medias = (studs1 * 2 + studs2 * 3 + studs3 * 5) / (2 + 3 + 5);
+            Console.WriteLine("MEDIAS = " + medias.ToString("F5"));
+            Console.ReadLine();
+            return medias;
+
+        }
+    }
+}
