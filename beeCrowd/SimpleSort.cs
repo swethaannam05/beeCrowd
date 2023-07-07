a70c78a18823dfef9d8ca2c662ad56255618aa53
swethaannam05 <annamswetharani@gmail.com>
2023-07-07 14:44:08 -0400

    Simple sort 2nd push

--- beeCrowd/SimpleSort.cs
+++ beeCrowd/SimpleSort.cs
@@ -19,7 +19,28 @@ namespace beeCrowd
 
             int small, middle, large;
 
-            if (num1 <= num2 && num1 <= num3)
+            //if (num1 > num2)
+            //{
+            //    int temp = num1;
+            //    num1 = num2;
+            //    num2 = temp;
+            //}
+
+            //if (num2 > num3)
+            //{
+            //    int temp = num2;
+            //    num2 = num3;
+            //    num3 = temp;
+
+            //    if (num1 > num2)
+            //    {
+            //        temp = num1;
+            //        num1 = num2;
+            //        num2 = temp;
+            //    }
+            //}
+
+                if (num1 <= num2 && num1 <= num3)
             {
                 small = num1;
                 if(num2<=num3)
